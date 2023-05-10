9a864cf85f58908009a7c86c26482c322ba518a2
Yong Seok Lee <lyeongsuk97@gmail.com>
2023-05-10 20:01:24 +0900

    Extract the data from GRE_vocabs.csv and save it as a dictionary

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import csv
+from collections import defaultdict
 
 if __name__ == '__main__':
 
     with open('GRE_vocabs.csv') as f:
         csv_reader = csv.reader(f)
-        columns = {}
+        first_row = next(csv_reader)
+        columns = defaultdict(list)
 
         for row in csv_reader:
-            print(row)
+            index = 1
+            for word in row:
+                if word == "":
+                    continue
+                columns[index].append(word)
+                index += 1
+
+    print(columns)
+
+
+    # 1. Create an index variable. As you iterate through the rows, increment it and add it t
